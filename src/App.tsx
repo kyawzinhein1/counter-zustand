6c629c2214aabe9432743bbbbfeeb050e32c4746
unknown <kyawzinhein162@gmail.com>
2024-11-23 08:31:52 +0630

    react-zustand-starter-kit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-cyan-600 text-center font-bold">
+        Hello Zustand
+      </h1>
+    </div>
+  );
+};
+
+export default App;
